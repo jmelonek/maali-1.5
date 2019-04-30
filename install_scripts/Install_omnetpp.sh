b8be0dbdfb883bc8e0a5d4ff39eba83c9f298062
Chris Bording <Chris.Bording@ibm.com>
2019-04-30 14:34:26 +0100

    add rpms tools maybe necessary for managing PSV
    Portable Storage Volume configuration

--- install_scripts/Install_omnetpp.sh
+++ install_scripts/Install_omnetpp.sh
@@ -12,7 +12,7 @@ sudo apt-get -y install xauth x11-apps libxt-dev
 # install nano xemacs emacs editors
 sudo apt-get -y install nano xemacs21 xemacs21-bin xemacs21-supportel xemacs21-basesupport-el
 
-sudo apt-get -y install emacs emacs24 emacs24-common emacs-goodies-el 
+sudo apt-get -y install emacs emacs25 emacs25-common emacs-goodies-el 
 
 # install tools that are needed for maali to work
 sudo apt-get -y install bc wget make environment-modules libtool
@@ -27,18 +27,19 @@ sudo apt-get -y install tcl-dev tk-dev libxml2-dev
 sudo apt-get -y install openjdk-11-jdk-headless openjdk-11-jre-headless openjdk-11-doc
 sudo apt-get -y install zlib1g-dev default-jre doxygen
 sudo apt-get -y install graphviz libwebkitgtk-1.0
-sudo apt-get -y install gfortran
+sudo apt-get -y install gfortran zip
 
 # firefox and tcl to install MATLAB
 sudo apt-get -y install firefox 
 
 # pkgs need to install omnet-5.2.1
-#sudo apt-get -y install libboost-all-dev
+sudo apt-get -y install libboost-all-dev
+sudo apt-get -y install multipath-tools
 #wget http://eclipse.bluemix.net/packages/2019-03/data/eclipse-inst-linux64.tar.gz
+curl -sL https://ibm.biz/idt-installer | bash
 
-
-if [ ! -d "/nectar" ]; then
-  sudo mkdir /nectar  
-  sudo chown -R ubuntu /nectar
-fi
+#if [ ! -d "/nectar" ]; then
+#  sudo mkdir /nectar  
+#  sudo chown -R ubuntu /nectar
+#fi
  
