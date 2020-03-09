6d4a727ac88f99a713e1b656409b6675ef97bdcb
Chris Bording <00079409@uwa.edu.au>
2020-03-09 15:54:16 +0800

    fixed typo in script and change nectar to nimbus

--- install_scripts/Install_maali_vagrant.sh
+++ install_scripts/Install_maali_vagrant.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# update your current image from the default repo 
+# update your current image from the default repo
 # This is will ensure that the remaining package management installs will work!
 #sudo apt-get -y update
 
 #sudo apt-get -y upgrade
 
 # install to allow x11 fowarding of gui interfaces
-sudo apt-get -y install xauth x11-apps libxt-dev 
+sudo apt-get -y install xauth x11-apps libxt-dev
 
 # install nano xemacs emacs editors
 sudo apt-get -y install nano xemacs21 xemacs21-bin xemacs21-supportel xemacs21-basesupport-el
 
-sudo apt-get -y install emacs emacs24 emacs24-common emacs-goodies-el 
+sudo apt-get -y install emacs emacs25 emacs25-common emacs-goodies-el
 
 # install tools that are needed for maali to work
 sudo apt-get -y install bc wget make environment-modules libtool
@@ -25,10 +25,9 @@ sudo apt-get -y install gfortran
 sudo apt-get -y install libncurses-dev
 
 # install libpython3.6-dev for python module in silo
-sudo apt-get =y install libpython3.6-dev
+sudo apt-get -y install libpython3.6-dev
 
-if [ ! -d "/nectar" ]; then
-  sudo mkdir /nectar 
-  sudo chown -R vagrant /nectar
+if [ ! -d "/nimbus" ]; then
+  sudo mkdir /nimbus
+  sudo chown -R vagrant /nimbus
 fi
- 
