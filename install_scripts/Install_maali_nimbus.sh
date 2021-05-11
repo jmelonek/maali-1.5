4371c5cff76343be42eedd1348e9a1d7a176dda5
Chris Bording <Chris.Bording@uwa.edu.au>
2021-05-11 22:34:25 +0800

    add rpms for R

--- install_scripts/Install_maali_nimbus.sh
+++ install_scripts/Install_maali_nimbus.sh
@@ -9,27 +9,25 @@ sudo apt-get -y update
 # install to allow x11 fowarding of gui interfaces
 sudo apt-get -y install xauth x11-apps libxt-dev
 
-# install nano xemacs emacs editors
-#sudo apt-get -y install nano xemacs21 xemacs21-bin xemacs21-supportel xemacs21-basesupport-el
-
 sudo apt-get -y install nano 
-#emacs emacs-common emacs-goodies-el
 
 # install tools that are needed for maali to work
-sudo apt-get -y install bc wget make environment-modules libtool
+sudo apt-get -y install bc wget make environment-modules libtool rpm
 
 # install the default gnu compilers
 # to find the the version number "gcc -v"
 sudo apt-get -y install gfortran g++
 
 # install libncurses-dev to support ccmake
-sudo apt-get -y install libncurses-dev
+sudo apt-get -y install libncurses-dev libcairo2-dev
 
 # install libpython3.8-dev for python module in silo
 sudo apt-get -y install libpython3.8-dev
 
 # install libcurl and libpcre for R
 sudo apt-get -y install libcurl4-gnutls-dev libpcre2-dev
+sudo apt-get -y install libpng-dev zlib-dev libjpeg-turbo8-dev
+sudo apt-get -y install libreadline-dev libbz2-dev liblzma-dev
 
 if [ ! -d "/nimbus" ]; then
   sudo mkdir /nimbus
