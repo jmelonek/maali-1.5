ce804b936cc256284e0c4bb1814d8cbf693ad982
Ubuntu <ubuntu@rbaseline.novalocal>
2021-05-11 15:20:49 +0000

    update zerotoR.sh for nimbus cloud

--- install_scripts/zerotoR.sh
+++ install_scripts/zerotoR.sh
@@ -2,17 +2,7 @@
 
 module load maali 
 
-maali -t openmpi -v 2.1.1 -c cloud -w 
+maali -t system-gcc -v 9.3.0 -c $1 -w 
 
-maali -t bzip2 -v 1.0.6 -c cloud -w 
-
-maali -t curl -v 7.54.0 -c cloud -w 
-
-maali -t zlib -v 1.2.8 -c cloud -w
-
-maali -t xz -v 5.2.3 -c cloud -w
-
-maali -t pcre -v 8.40 -c cloud -w 
-
-maali -t R -v 3.3.3 -c cloud -w 
+maali -t R -v 4.0.5 -c $1 -w 
 
